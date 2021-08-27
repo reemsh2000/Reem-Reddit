2db3da45d11900fc23e8977404ed30baaa0623c9
reemsh2000 <sh951652@gmail.com>
2021-08-27 12:10:00 +0300

    edit in word

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const port = app.get('port');
 app.listen(port, () => {
   // eslint-disable-next-line no-console
-  console.log(`app is  listining on http://localhost:${port}`);
+  console.log(`app is  listening on http://localhost:${port}`);
 });
