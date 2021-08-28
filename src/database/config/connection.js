e59209917ccbef230fc053d35c5ce894cce6fad3
reemsh2000 <sh951652@gmail.com>
2021-08-28 13:19:57 +0300

    create connection and database Relates #6

---
+++ src/database/config/connection.js
@@ -0,0 +1,30 @@
+const { Pool } = require('pg');
+require('env2')('.env');
+
+const {
+  NODE_ENV, DEV_DB_URL, DATABASE_URL, TEST_DB_URL,
+} = process.env;
+
+let dbUrl = '';
+
+switch (NODE_ENV) {
+  case 'production':
+    dbUrl = DATABASE_URL;
+    break;
+  case 'development':
+    dbUrl = DEV_DB_URL;
+    break;
+  case 'testing':
+    dbUrl = TEST_DB_URL;
+    break;
+  default:
+    throw new Error('database doesn\'t exist');
+}
+
+const options = {
+  connectionString: dbUrl,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
+module.exports = new Pool(options);
