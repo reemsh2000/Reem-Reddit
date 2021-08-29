70971b882a2b63f793a6f104b8b0dd96d4b470e0
reemsh2000 <sh951652@gmail.com>
2021-08-29 10:04:13 +0300

    handle error pages

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cookieParser = require('cookie-parser');
 const express = require('express');
 const { join } = require('path');
 const router = require('./router');
+const { clientError, serverError } = require('./controllers/error');
 
 const app = express();
 app.set('port', process.env.PORT || 6060);
@@ -12,5 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(join(__dirname, '..', 'public')));
 app.use(router);
-
+app.use(clientError);
+app.use(serverError);
 module.exports = app;
