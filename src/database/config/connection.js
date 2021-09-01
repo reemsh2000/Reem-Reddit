e1edebc8606cc09550fcb21732e813b2fabe31d5
reemsh2000 <sh951652@gmail.com>
2021-09-01 23:15:15 +0300

    remove comments from connection file

--- src/database/config/connection.js
+++ src/database/config/connection.js
@@ -7,22 +7,22 @@ const {
 
 let dbUrl = '';
 
-// switch (NODE_ENV) {
-//   case 'production':
-//     dbUrl = DATABASE_URL;
-//     break;
-//   case 'development':
-//     dbUrl = DEV_DB_URL;
-//     break;
-//   case 'testing':
-//     dbUrl = TEST_DB_URL;
-//     break;
-//   default:
-//     throw new Error('database doesn\'t exist');
-// }
+switch (NODE_ENV) {
+  case 'production':
+    dbUrl = DATABASE_URL;
+    break;
+  case 'development':
+    dbUrl = DEV_DB_URL;
+    break;
+  case 'testing':
+    dbUrl = TEST_DB_URL;
+    break;
+  default:
+    throw new Error('database doesn\'t exist');
+}
 
 const options = {
-  connectionString: DEV_DB_URL,
+  connectionString: dbUrl,
   ssl: {
     rejectUnauthorized: false,
   },
