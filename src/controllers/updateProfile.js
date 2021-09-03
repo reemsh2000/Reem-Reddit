3a6b7b3b4d0142f50b26c5c4d9e56cc0f3caba84
reemsh2000 <sh951652@gmail.com>
2021-09-03 14:07:15 +0300

    edit site title and fic profile page error

--- src/controllers/updateProfile.js
+++ src/controllers/updateProfile.js
@@ -4,11 +4,16 @@ const verifyLogin = require('./verifyLogin');
 
 const updateProfile = (req, res) => {
   const { newpassword, newProfileImage, newUserEmail } = req.body;
-  const verify = verifyLogin(req, res);
-  const { userName } = verify;
-  hashPassword(newpassword).then((password) => {
-    updateUser(userName, newProfileImage, password, newUserEmail);
-    res.redirect('/profile');
+  verifyLogin(req, res, (err, decoded) => {
+    if (err) {
+      res.json({ msg: 'you are not verified' });
+    } else {
+      const { userName } = decoded;
+      hashPassword(newpassword).then((password) => {
+        updateUser(userName, newProfileImage, password, newUserEmail);
+        res.redirect(`/profile/${userName}`);
+      });
+    }
   });
 };
 module.exports = updateProfile;
